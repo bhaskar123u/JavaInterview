d69e9ade4f66bad11c38f9ebde0efdb2d4e22f41
Bhaskar Sharan <bhaskar123u@gmail.com>
2024-10-24 09:36:47 +0530

    improving sigleton

--- src/Singleton.java
+++ src/Singleton.java
@@ -1,20 +1,53 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.lang.reflect.Constructor;
+
 public class Singleton {
-    public static void main(String[] args) throws CloneNotSupportedException{
-        /*SingletonImpl obj1 = SingletonImpl.getInstance();
+    public static void main(String[] args) throws CloneNotSupportedException, FileNotFoundException, IOException, ClassNotFoundException{
+        SingletonImpl obj1 = SingletonImpl.getInstance();
         SingletonImpl obj2 = SingletonImpl.getInstance();
         System.out.println(obj1.hashCode());
-        System.out.println(obj2.hashCode());*/
+        System.out.println(obj2.hashCode());
 
-
-        /*SingletonImpl instance1 = SingletonImpl.getInstance();
+        // Trying to create instance using clone
+        SingletonImpl instance1 = SingletonImpl.getInstance();
         SingletonImpl instance2 = (SingletonImpl) instance1.clone();
-        System.out.println("instance1 hashCode:- "+ instance1.hashCode());
-        System.out.println("instance2 hashCode:- "+ instance2.hashCode());*/
+        System.out.println("Instance 1 hashCode (cloning): " + instance1.hashCode());
+        System.out.println("Instance 2 hashCode (cloning): " + instance2.hashCode());
 
-        
+        // Serialize the singleton instance
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("singleton.ser"))) {
+            oos.writeObject(obj1);
+        }
+
+        // Deserialize the singleton instance
+        SingletonImpl deserializedInstance;
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("singleton.ser"))) {
+            deserializedInstance = (SingletonImpl) ois.readObject();
+        }
+
+        System.out.println("Deserialized Instance hashCode: " + deserializedInstance.hashCode());
+        System.out.println("Are both instances equal? " + (obj1 == deserializedInstance));
+
+        // Attempt to create an object using reflection
+        try {
+            Constructor<SingletonImpl> constructor = SingletonImpl.class.getDeclaredConstructor();
+            constructor.setAccessible(true);  // Bypass the private constructor access
+            SingletonImpl reflectionInstance = constructor.newInstance();  // Create new instance
+            System.out.println("Reflection instance hashCode: " + reflectionInstance.hashCode());
+        } catch (Exception e) {
+            System.out.println("Reflection failed: " + e.getMessage());
+        }
     }
 }
-class SingletonImpl implements Cloneable{
+
+class SingletonImpl implements Cloneable, Serializable {
+    //private static final long serialVersionUID = 1L;
     private static volatile SingletonImpl obj  = null;
 
     private SingletonImpl() {
